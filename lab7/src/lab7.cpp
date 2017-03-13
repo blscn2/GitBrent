7c64e0e55543fe31819edc0086d86b6bf830d566
Brent <blscn2@mail.missouri.edu>
2017-03-13 17:11:51 -0500

    lab7

---
+++ lab7/src/lab7.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lab7.cpp
+// Author      : Brent Schultz
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "lab6" << endl; // prints lab6
+	return 0;
+}
