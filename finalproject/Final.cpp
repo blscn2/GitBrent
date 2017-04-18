961c4c3e93ee12f8a7d660913a06ff12fa480dbe
Brent <blscn2@mail.missouri.edu>
2017-04-17 21:26:42 -0500

    1a

--- finalproject/Final.cpp
+++ finalproject/Final.cpp
@@ -1,1 +1,21 @@
-Final Project??
+//============================================================================
+// Name        : Project
+// Author      : Brent Schultz, Shayne Wadle
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Final Project
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include <vector>
+using namespace std;
+
+
+class Customer{
+	private:
+
+	public:
+}
