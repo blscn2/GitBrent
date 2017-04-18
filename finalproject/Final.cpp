d9ba18ead2dbe97e756af8e8b19e7ea8fbd5f783
Brent <blscn2@mail.missouri.edu>
2017-04-17 21:53:27 -0500

    1d

--- finalproject/Final.cpp
+++ finalproject/Final.cpp
@@ -19,16 +19,28 @@ class Customer{
 		string name = "John Doe";
 		int accountnumber = 123456;
 		double balance = 356.25;
+		string accounttype = "Standard or student or loyal";
 
 	public:
 		virtual void printInfo(void);
+		void Withdrawal();
+		void Deposit();
+		void Invest();
+
+		void CloseAccount();
+		~Customer();
 
 }
 
 void Customer::printInfo(void){
 	cout << "Account holder name: " << name << endl;
 	cout << "Account number: " << accountnumber << endl;
-	cout << "Account baland: $" << balance << endl;
+	cout << "Account balance: $" << balance << endl;
+	cout << "Account type: " << accounttype << endl;
+}
+
+Customer::~Customer(){
+
 }
 
 int main(int argc, char* argv[])
