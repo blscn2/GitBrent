1bc0411984c225266b63100b99d61adfe0a31e60
Brent <blscn2@mail.missouri.edu>
2017-04-24 17:00:03 -0500

    Add comments.

--- lab10/src/lab10.cpp
+++ lab10/src/lab10.cpp
@@ -3,7 +3,7 @@
 // Author      : Brent Schultz
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Lab 10
 //============================================================================
 
 #include <stdio.h>
@@ -14,532 +14,200 @@
 using namespace std;
 
 // abstract base class for conic sections
-
 class BaseConic{
-	private:
+	protected:
 		double x;
 		double y;
-
+		double radius1;
 
 	public:
-		BaseConic(double x0=0, double y0=0){...};
-		virtual ~BaseConic(){};
-		void Move(double nx, double ny){x=nx; y=ny;}
+		BaseConic(double x0 = 0.0, double y0 = 0.0);
+		virtual ~BaseConic();
+		virtual void Display();
+		void Move(double nx, double ny);
 		virtual double Area() const = 0; //PURE VIRTUAL FUNCTION which makes this an ABC
+
 };
 
-class BaseConic::Ellipse{
+void BaseConic::Move(double nx, double ny){
+	x = nx + x;
+	y = nx + y;
+}
+
+void BaseConic::Display(){
+	cout << "Coordinates: (" << x << ", " << y << ")" << endl
+	 << "Radius: " << radius1 << endl
+	 << "Area of shape: " << Area() << " units sq \n" << endl;
+}
+
+BaseConic::BaseConic(double x0, double y0){
+	x = x0;
+	y = y0;
+	radius1 = 0;
+}
+
+BaseConic::~BaseConic(){
+
+}
+
+class Ellipse: public BaseConic{
 	private:
-		double x;
-		double y;
-		double a;
-		double b;
-		double angle;
+		double radius2;
+		double theta;
 
 	public:
-		void Move(double nx, double ny){x=nx; y=ny;}
-		virtual void Rotate(double nang){angle += nang;}
+		void Rotate(double nang);
+		void Vertices();
+		void Resize(double scale1, double scale2);
+		void Display();
+		double Area() const {return 3.14 * radius1 * radius2;}
 
-
+		Ellipse();
+		Ellipse( double x0, double y0);
+		virtual ~Ellipse();
 };
 
-class BaseConic::Circle{
-	private:
-		double x;
-		double y;
-		double radius;
+void Ellipse::Rotate( double nang){
+	theta += nang;
+	if(theta >= 360){
+		theta -= 360;
+	}
+}
 
+void Ellipse::Vertices(){
+	int x1, x2, y1, y2;
+	x1 = x + radius1;
+	x2 = x - radius1;
+	y1 = y + radius2;
+	y2 = y - radius2;
+
+	cout << "The ellipses vertices are:" << endl
+			<< "(" << x1 << ",0)"<< endl
+			<< "(" << x2 << ",0)"<< endl
+			<< "(0," << y1 << ")"<< endl
+			<< "(0," << y2 << ")\n" << endl;
+
+}
+
+void Ellipse::Resize(double scale1, double scale2){
+	radius1 *= scale1;
+	radius2 *= scale2;
+}
+
+void Ellipse::Display(){
+	cout << "Coordinates: (" << x << ", " << y << ")" << endl
+		 << "XRadius: " << radius1 << endl
+		 << "YRadius: "<< radius2 << endl
+		 << "Area of shape: " << Area() << " units sq \n" << endl;
+}
+
+Ellipse::Ellipse(){
+	radius1 = 1;
+	theta = 0;
+}
+
+Ellipse::Ellipse( double x0, double y0){
+	radius1 = x0;
+	radius2 = y0;
+	theta =1;
+}
+
+Ellipse::~Ellipse(){
+
+}
+
+class Circle: public BaseConic{
 	public:
-		void Move(double nx, double ny){x=nx; y=ny;}
-		virtual double Area() const {return 3.14159 * radius * radius;}
+		int InCircle( double xc, double yc);
+		virtual double Area() const {return 3.14 * radius1 * radius1;}
 
+		void Resize(double scale);
+		Circle();
+		Circle( double rad);
+		Circle( double x0, double y0);
+		~Circle();
 };
 
+int Circle::InCircle( double xc, double yc){
+	if( (xc <= x + radius1 && xc >= x - radius1) && (yc <= y + radius1 && yc >= y - radius1)){
+		return 1;
+	}
+	else
+		return 0;
+}
 
+void Circle::Resize(double scale){
+	radius1 *= scale;
+}
 
-// class for a signal
-class Signal{					// FIX MAX READ IN  AND UPDATE CURRENT VECTOR
-	// private members
+Circle::Circle(){
 
-	private:
-//		int max;
-//		double average;
-//		//double *data = NULL;
-//		vector<double>data;
-//		int length;
-//		vector<double>array;
-//		//double *array = NULL;
-//		int check;
+}
 
-	// public members and methods
-	public:
+Circle::Circle(double rad){
+	radius1 = rad;
+}
 
-		int max;
-		double average;
-		int length;
-		//double *data = NULL;		replaced by next line
-		vector<double>data;
-		vector<double>array;
-		//double *array = NULL;		replaced by previous line
-		int check;
+Circle::Circle(double x0, double y0){
 
+}
 
-		// open file, read data into private variables, close file
-		Signal();				// receives nothing
-		Signal(int); 			// receives file number
-		Signal(const char *); 	// receives filename
-		~Signal();
+Circle::~Circle(){
 
-		void operator+(double); // overloaded operator for offset
-		void operator*(double); // overloaded operator for scale
+}
 
-		void center(Signal);
-		void normalize(Signal);
-		void statistics();
+int main(){
 
-		void sig_info();
-		void save_file(int,int);
-		void print_signal(vector<double>);
-		void mean();
-		int check_file_success();
-
-
-};
-
-// Verify that the file opened
-int Signal::check_file_success(){
-	//cout << data << endl;
-	if(data.empty()){
-		// File did not load correctly
-		return -1;
-	}else{
-		// file loaded correctly
+	double rad1;
+	cout<<"Enter radius to be used " <<endl;
+	cin >> rad1;
+	if ( rad1 <= 0){
+		cout << "Using 0 or negative radii is unacceptable " << endl;
 		return 0;
 	}
-}
+	Circle circle1(rad1);
+	cout << "Original Circle: " << endl;
+	circle1.Display();
+	circle1.Move(5,5);
+	cout << "After Moving: " << endl;
+	circle1.Display();
+	circle1.Resize(5);
+	cout << "After Resize: " << endl;
+	circle1.Display();
 
-// This function writes the transformed signal to a file
-void Signal::save_file(int flag, int num){
-	char filename[50];
+	int xcord = 20;
+	int ycord = 20;
 
-	if(flag == 0){
-		cout << "No transformations have been done." << endl;
-		return;
-	// if an offset was done
-	}else if(flag == 1){
-		if(num<9){ // includes a zero in the name
-			sprintf(filename,"Offset_data_0%d.txt",num);
-		}else{ //doesn't include a zero in the name
-			sprintf(filename,"Offset_data_%d.txt",num);
+	if ( 1 == circle1.InCircle(xcord, ycord) ){
+		cout << "Point (" << xcord << "," << ycord <<") is inside circle \n" << endl;
+	}
+	else if ( 0 == circle1.InCircle(xcord, ycord) ){
+		cout << "Point (" << xcord << "," << ycord <<") is not inside circle \n" << endl;
+	}
+
+
+	double rad2;
+	cout<<"Enter radius to be used " <<endl;
+	cin >> rad2;
+	if ( rad2 <= 0){
+			cout << "Using 0 or negative radii is unacceptable " << endl;
+			return 0;
 		}
-	// if a scale was done
-	}else if(flag == 2){
-		if(num<9){ // includes a zero in the name
-			sprintf(filename,"Scale_data_0%d.txt",num);
-		}else{ //doesn't include a zero in the name
-			sprintf(filename,"Scale_data_%d.txt",num);
-		}
-	// if center was done
-	}else if(flag == 3){
-		if(num<9){ // includes a zero in the name
-			sprintf(filename,"Center_data_0%d.txt",num);
-		}else{ //doesn't include a zero in the name
-			sprintf(filename,"Center_data_%d.txt",num);
-		}
-	// if normalize was done
-	}else{
-		if(num<9){ // includes a zero in the name
-			sprintf(filename,"Normalize_data_0%d.txt",num);
-		}else{ //doesn't include a zero in the name
-			sprintf(filename,"Normalize_data_%d.txt",num);
-		}
-	}
+	Ellipse ellipse1(rad1, rad2);
+	cout << "Original Ellipse: " << endl;
+	ellipse1.Display();
+	ellipse1.Vertices();
+	ellipse1.Move(5,5);
+	cout << "After Moving: " << endl;
+	ellipse1.Display();
+	ellipse1.Resize(2,3);
+	cout << "After Resize: " << endl;
+	ellipse1.Display();
+	ellipse1.Rotate(30);
+	cout << "After Rotate: " <<  endl;
+ 	ellipse1.Display();
 
-	cout << "Writing new signal to file." << endl;
-	int i;
-
-	// Open the file and write the signal to the file
-	FILE *fp = fopen(filename,"w");
-	if(fp == NULL){
-		printf("ERROR opening file\n");
-	}else{
-		fprintf(fp,"%d %d\n",length,max);
-		for(i=0;i<length;i++){
-			fprintf(fp,"%.4f\n",array[i]);
-		}
-		fclose(fp);
-	}
-}
-
-// Display signal info
-void Signal::sig_info(){
-	cout << "Signal Info\n\tMax: ";
-	cout << max << endl;
-	cout << "\n\tLength: ";
-	cout << length << endl;
-	cout << "\n\tAverage: ";
-	cout << average << endl;
-}
-
-// This method prints an array of doubles
-void Signal::print_signal(vector<double>x){
-
-	int i;
-	for(i=0;i<length;i++){
-		cout << x[i];
-		cout << " ";
-	}
-}
-
-// This function generates a file containing the mean and max of the raw signal
-void Signal::statistics(){
-	char file_name[] = "Statistics_File.txt";
-	FILE * fp = fopen(file_name,"w");
-	fprintf(fp,"%.4f %d",average,max);
-	cout << "Mean and max written to file" << endl;
-	cout << "The mean is: ";
-	cout << average << endl;
-	cout << "The max is: ";
-	cout << max << endl;
-	fclose(fp);
-}
-
-// This function calculates the average of the raw signal
-void Signal::mean(){
-	double total = 0.0;
-	int i=0;
-	for(i=0;i<length;i++){
-		total = total+data[i];
-	}
-	average = total/(double)length; //length is type int
-}
-
-// This function offsets the signal based on an offset value inputted
-void Signal::operator+(double offset_value){
-	cout << "Offset started" << endl;
-	int i;
-
-	if(array.empty()){
-		for(i=0;i<length;i++){
-			array.push_back(data[i] + offset_value);
-		}
-	}else{
-		for(i=0;i<length;i++){
-			array[i] = data[i] + offset_value;
-		}
-	}
-	cout << "The signal now is: ";
-	print_signal(array);
-}
-
-//This function scales the signal based on a scale factor inputted
-void Signal::operator*(double scale_factor){
-	cout << "Scale started" << endl;
-
-	int i;
-
-	if(array.empty()){
-		for(i=0;i<length;i++){
-			array.push_back(data[i] * scale_factor);
-		}
-	}else{
-		for(i=0;i<length;i++){
-			array[i] = data[i] * scale_factor;
-		}
-	}
-	cout << "The signal now is: ";
-	print_signal(array);
-}
-
-
-//This function centers the signal
-void Signal::center(Signal x){
-	// Since centering is the same as offsetting the signal by a factor
-	// of -mean, the offset can be used with an offset value of -mean
-	cout << "Centering data" << endl;
-	x + (-average);
-}
-
-// This function normalizes the singal
-void Signal::normalize(Signal x){
-	// Since normalizing is the same as scaling the signal by a factor
-	// of 1/max, the scaleSignal() function can be used with a scale value of 1/max
-	cout << "Normalizing data" << endl;
-
-	x * (1/(double)max);
-}
-
-// This is the first constructor which takes no parameters
-// and initializes all members to zero or NULL
-Signal::Signal(){
-
-	max = 0;
-	//data = NULL;
-	length = 0;
-	average = 0;
-	//array = NULL;
-}
-
-// This is the second constructor that takes in the file number
-// and reads in the data from the file
-Signal::Signal(int file){
-	double cell;
-	int i;
-	char file_name[50];
-
-	// form the file name using the file number
-	if(file<9){
-		sprintf(file_name,"Raw_data_0%d.txt",file);
-	}else{
-		sprintf(file_name,"Raw_data_%d.txt",file);
-	}
-
-	//open file
-	FILE *fp = fopen(file_name,"r");
-	if (fp == NULL){
-		return;
-	}
-
-	// read in contents
-	fscanf(fp,"%d %d",&length,&max);
-	//data = new double [length];
-	for(i=0;i<length;i++){
-		fscanf(fp,"%lf",&cell);
-		data.push_back(cell);
-
-	}
-
-	// Display the signal
-	cout << "The signal from file is: ";
-	print_signal(data);
-	fclose(fp);
-}
-
-// This is the third constructor that takes in the file name
-// and reads in the data from the file
-Signal::Signal(const char * file_name){
-	double cell;
-	int i;
-
-	// open file
-	FILE *fp = fopen(file_name,"r");
-	if (fp == NULL){
-		return;
-	}
-	// read in contents
-	fscanf(fp,"%d %d",&length,&max);
-	//data = new double [length];
-	for(i=0;i<length;i++){
-		fscanf(fp,"%lf",&cell);
-		data.push_back(cell);
-	}
-	//display signal
-	cout << "The signal from file is: ";
-	print_signal(data);
-	fclose(fp);
-}
-
-// This is the destructor
-Signal::~Signal(){
-	// nothing to be deleted here because use of vectors
-}
-
-Signal operator+(Signal &one, Signal &two){
-
-	Signal three;
-	if( one.length != two.length){ //Error checking to make sure they are same length
-		cout << "\nSignals are not the same length. Exiting." << endl;
-		return three;
-	} else {
-	// do element additions between one and two and then assign
-	// those values to three's data
-	for(int i=0;i<one.data.size();i++){
-		three.data.push_back(one.data[i] + two.data[i]);
-	}
-	cout << endl;
-
-	// get the max
-	three.max = 1;
-
-	double total;
-
-	// print the new signals data and also calculate the average
-	cout << "Adding two signals (Raw_data_01.txt and Raw_data_01.txt by default)." << endl;
-	cout << "The new signal's data vector is: ";
-	for(int i=0;i<three.data.size();i++){
-		total+=three.data[i];
-		cout << three.data[i];
-		cout << " ";
-		if(three.data[i] > three.max){
-			three.max = three.data[i];
-		}
-	}
-
-	three.average = total/three.data.size();
-
-	cout << "\nThe max is: " << three.max << endl;      //display max from original signals
-	cout << "The average is: " << three.average << endl; // original
-
-
-	return three;
-	}
-}
-
-int main(int argc, char *argv[]){
-	char filename[50];
-	int filenumber = 0;
-	int choice, i;
-	int flag = 0; // for which transformation is done
-
-	// if no command line arguments are given
-	if(argc == 1){
-		// as the user to enter the filename
-		cout << "No arguments given" << endl;
-		cout << "Enter filename> " << endl;
-		cin >> filename;
-	}
-	// if command line arguments are given
-	else if(argc == 3){
-		// if -n is given
-		if(strcmp(argv[1],"-n") == 0){
-
-			// check that a number is given
-			if(isdigit(*argv[2])){
-				filenumber = atoi(*(argv+2));
-			}else{
-				cout << "ERROR: '-n' expects a number not a string\nExiting.\n";
-				return -1;
-			}
-			//cout << filenumber;
-		}
-		// if -f is given
-		else if(strcmp(argv[1],"-f") == 0){
-			cout << "File name given" << endl;
-
-			if(isdigit(*argv[2])){
-				// check that a string is given and not a number
-				cout << "ERROR: '-f' expects a string not a number\nExiting.\n";
-				return -1;
-			}else{
-				strcpy(filename,argv[2]);
-			}
-		}
-	// otherwise the input is incorrect
-	}else{
-		cout << "ERROR with arguments\nExpecting:\n\t'./a.out -n filenumber'\n\t'./a.out -f filename.txt'\n" << endl;
-		return -1;
-	}
-
-	// initialize the object signal
-	Signal sig1;
-	// if a filenumber was given call the appropriate constructor
-	if(filenumber != 0){
-		sig1 = Signal(filenumber);
-		// check that the file was able to be read from
-		if(sig1.check_file_success() == -1){
-			cout << "File given was not found.\nExiting.\n";
-			return -1;
-		}
-	// otherwise a filename was given
-	}else{
-		// call the constructor
-		sig1 = Signal(filename);
-		// check that the file was able to be read from
-		if(sig1.check_file_success() == -1){
-			cout << "File given was not found.\nExiting.\n";
-			return -1;
-		}
-	}
-
-	//get file number if filename is given
-	if(filenumber == 0){
-		char *split;
-		split = strtok (filename,"_.");
-    	split = strtok (NULL, "_.");
-    	split = strtok(NULL, "_.");
-    	filenumber = atoi(split);
-    }
-
-    // find the average of the signal
-    sig1.mean();
-
-
-    // User menu
-	while(1){
-		// ask the user to pick what they would like to do
-		cout << "\nSelect:\n(0) Get Info\n(1) Offset\n(2) Scale\n(3) Center\n(4) Normalize\n(5) Statistic\n(6) Save To File\n(7) Exit\n(8) Add two Signals\n> ";
-		cin >> choice;
-		while( (choice > 8) | (choice < 0) ){
-			cout <<"Invalid. Enter again: ";
-			cin >> choice;
-		}
-
-		// based on their choice, call the correct method
-		switch(choice){
-			case 0:{
-				// display info
-				sig1.sig_info();
-				break;}
-			case 1: {
-				// do offset
-				flag = 1;
-				double offset_num = 0.0;
-				cout << "Enter offset value: ";
-				cin >> offset_num;
-				cout << "Calling offset" << endl;
-				//sig1.offset(offset_num);
-				sig1+offset_num;
-				break;}
-			case 2: {
-				// do scale
-				flag = 2;
-				double scale_num = 0.0;
-				cout << "Enter a scale factor: ";
-				cin >> scale_num;
-				//sig1.scale(scale_num);
-				sig1*scale_num;
-				break;}
-
-			case 3:{
-				// do center
-				flag = 3;
-				sig1.center(sig1);
-				break;}
-
-			case 4:{
-				// do normalize
-				flag = 4;
-				sig1.normalize(sig1);
-				break;}
-			case 5:{
-				// create statistics file
-				sig1.statistics();
-				break;}
-			case 6:{
-				// save signal to file
-				sig1.save_file(flag,filenumber);
-				break;}
-
-			case 7:{
-				// exit program
-				return 0;
-				break;}
-			case 8:{
-				// this is for the non-member addition operator
-				Signal sig2("Raw_data_01.txt");
-				Signal sig3("newdata.txt");
-				Signal added;
-
-				added = operator+(sig2,sig3);
-				break;
-				}
-			default: {
-				cout << "Invalid choice. Try again." << endl;
-				cin >> choice;
-				break;
-				}
-
-		}
-	}
 	return 0;
 }
+
+
